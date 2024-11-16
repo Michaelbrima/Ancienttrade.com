cf20b3d976a93c4443377305550fb8e8e9b84595
Michaelbrima <157252258+Michaelbrima@users.noreply.github.com>
2024-11-16 13:18:23 -0500

    Update index.php
    Added 'thead' object as well as second entry into 'hats' table.

--- api/index.php
+++ api/index.php
@@ -13,13 +13,25 @@
 	<h1>List of Hats Available</h1>
 	<br>
 	<table>
-		<tr>
-			<th>id</th>
-			<th>Title</th>
-			<th>Description</th>
-			<th>Price</th>
-			<th>Image</th>
-		</tr>
+		<thead>
+			<tr>
+				<th>id</th>
+				<th>Title</th>
+				<th>Description</th>
+				<th>Price</th>
+				<th>Image</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<tr>
+				<td>2</td>
+				<td>green hat</td>
+				<td>A nice, green colored hat with a good texture.</td>
+				<td>2.00</td>
+				<td>image</td>
+			</tr>
+		</tbody>
 	</table>
 </body>
 
